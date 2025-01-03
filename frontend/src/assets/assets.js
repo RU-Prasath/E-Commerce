0bf3fc069079d9ed812a6fc692b4e66da38dc1b4
prasath-hepl <prasath.ru@hepl.com>
2025-01-03 12:14:58 +0530

    first-commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,350 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import collection_1 from './watch_3.png'
+import collection_2 from './coolingGlass_4.png'
+import collection_3 from './pant_1.png'
+import collection_4 from './shirt_2.png'
+import collection_5 from './wallet_3.png'
+import collection_6 from './shoe_4.png'
+import collection_7 from './shorts_1.png'
+import collection_8 from './T-shirt_2.png'
+
+import watch_1 from './watch_1.png'
+import watch_2 from './watch_2.png'
+import watch_3 from './watch_3.png'
+import watch_4 from './watch_4.png'
+import coolingGlass_1 from './coolingGlass_1.png'
+import coolingGlass_2 from './coolingGlass_2.png'
+import coolingGlass_3 from './coolingGlass_3.png'
+import coolingGlass_4 from './coolingGlass_4.png'
+import pant_1 from './pant_1.png'
+import pant_2 from './pant_2.png'
+import pant_3 from './pant_3.png'
+import pant_4 from './pant_4.png'
+import shirt_1 from './shirt_1.png'
+import shirt_2 from './shirt_2.png'
+import shirt_3 from './shirt_3.png'
+import shirt_4 from './shirt_4.png'
+import wallet_1 from './wallet_1.png'
+import wallet_2 from './wallet_2.png'
+import wallet_3 from './wallet_3.png'
+import wallet_4 from './wallet_4.png'
+import shoe_1 from './shoe_1.png'
+import shoe_2 from './shoe_2.png'
+import shoe_3 from './shoe_3.png'
+import shoe_4 from './shoe_4.png'
+import shorts_1 from './shorts_1.png'
+import shorts_2 from './shorts_2.png'
+import shorts_3 from './shorts_3.png'
+import shorts_4 from './shorts_4.png'
+import t_shirt_1 from './T-shirt_1.png'
+import t_shirt_2 from './T-shirt_2.png'
+import t_shirt_3 from './T-shirt_3.png'
+import t_shirt_4 from './T-shirt_4.png'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Watch",
+        menu_image: collection_1
+    },
+    {
+        menu_name: "CoolingGlass",
+        menu_image: collection_2
+    },
+    {
+        menu_name: "Pant",
+        menu_image: collection_3
+    },
+    {
+        menu_name: "Shirt",
+        menu_image: collection_4
+    },
+    {
+        menu_name: "Wallet",
+        menu_image: collection_5
+    },
+    {
+        menu_name: "Shoe",
+        menu_image: collection_6
+    },
+    {
+        menu_name: "Shorts",
+        menu_image: collection_7
+    },
+    {
+        menu_name: "T-shirt",
+        menu_image: collection_8
+    }]
+
+export const product_list = [
+    {
+        _id: "1",
+        name: "Rolex Watch",
+        image: watch_1,
+        price: 15500,
+        description: "Rolex watch is a luxurious, precision-engineered timepiece is now for sale",
+        category: "Watch"
+    },
+    {
+        _id: "2",
+        name: "Quartz Watch",
+        image: watch_2,
+        price: 311000,
+        description: "A stylish and reliable Quartz Watch, offering precise timekeeping and modern design for everyday wear",
+        category: "Watch"
+    }, {
+        _id: "3",
+        name: "Poedagar Watch",
+        image: watch_3,
+        price: 5000,
+        description: "The Poedagar Watch offers modern design and precision craftsmanship in a stylish, reliable timepiece",
+        category: "Watch"
+    }, {
+        _id: "4",
+        name: "Kanchan Watch",
+        image: watch_4,
+        price: 24000,
+        description: "Discover the Kanchan Watch: a blend of timeless elegance and modern craftsmanship",
+        category: "Watch"
+    }, {
+        _id: "5",
+        name: "Luxury Glass",
+        image: coolingGlass_1,
+        price: 140,
+        description: "Stay cool in style with our fashionable Cooling Glasses for sunny days",
+        category: "CoolingGlass"
+    }, {
+        _id: "6",
+        name: "Plastic Glass",
+        image: coolingGlass_2,
+        price: 120,
+        description: "Beat the heat with our sleek Cooling Glasses featuring UV protection",
+        category: "CoolingGlass"
+    }, {
+        _id: "7",
+        name: "Cooling Glass",
+        image: coolingGlass_3,
+        price: 200,
+        description: "Shield your eyes in style with our fashionable and functional Cooling Glasses",
+        category: "CoolingGlass"
+    }, {
+        _id: "8",
+        name: "Green Glass",
+        image: coolingGlass_4,
+        price: 150,
+        description: "Step out in sunny weather with our stylish Cooling Glasses for all-day wear",
+        category: "CoolingGlass"
+    }, {
+        _id: "9",
+        name: "Ankle Pant",
+        image: pant_1,
+        price: 400,
+        description: "Modern cut that hits at the ankle, offering a trendy look",
+        category: "Pant"
+    }, { 
+        _id: "10",
+        name: "Formal Pant",
+        image: pant_2,
+        price: 500,
+        description: "Elevate your wardrobe with our stylish Formal Pant, perfect for business occasions",
+        category: "Pant"
+    }, {
+        _id: "11",
+        name: "Casual Pant",
+        image: pant_3,
+        price: 1000,
+        description: "Embrace laid-back style with our versatile Casual Pant, ideal for everyday wear",
+        category: "Pant"
+    }, {
+        _id: "12",
+        name: "Lycra Pant",
+        image: pant_4,
+        price: 399,
+        description: "Experience comfort and style with our flexible Lycra Pant, perfect for modern looks",
+        category: "Pant"
+    },
+    {
+        _id: "13",
+        name: "Casual Shirt",
+        image: shirt_1,
+        price: 400,
+        description: "Laid-back charm with refined style, perfect for relaxed days",
+        category: "Shirt"
+    },
+    {
+        _id: "14",
+        name: "Formal Shirt",
+        image: shirt_2,
+        price: 600,
+        description: "Elevate your wardrobe with our sophisticated, premium fabric Formal Shirt",
+        category: "Shirt"
+    }, {
+        _id: "15",
+        name: "Double Pocket",
+        image: shirt_3,
+        price: 800,
+        description: "Introducing our practical Double Pocket Shirt with dual front pockets",
+        category: "Shirt"
+    }, {
+        _id: "16",
+        name: "Denim Shirt",
+        image: shirt_4,
+        price: 700,
+        description: "Add rugged charm with our durable Denim Shirt, perfect for casual appeal",
+        category: "Shirt"
+    }, {
+        _id: "17",
+        name: "Classic Wallet",
+        image: wallet_1,
+        price: 280,
+        description: "Our Classic Wallet offers ample card slots in a sleek design",
+        category: "Wallet"
+    }, {
+        _id: "18",
+        name: "Leather Wallet",
+        image: wallet_2,
+        price: 400,
+        description: "Elevate your essentials with our genuine Leather Wallet",
+        category: "Wallet"
+    }, {
+        _id: "19",
+        name: "Design Wallet",
+        image: wallet_3,
+        price: 300,
+        description: "Discover our Designer Wallet crafted from premium leather",
+        category: "Wallet"
+    }, {
+        _id: "20",
+        name: "Brown Wallet",
+        image: wallet_4,
+        price: 150,
+        description: "Explore our Brown Wallet crafted from rich, genuine leather",
+        category: "Wallet"
+    }, {
+        _id: "21",
+        name: "Leather Shoe",
+        image: shoe_1,
+        price: 4400,
+        description: "Premium leather shoes for timeless style and comfort",
+        category: "Shoe"
+    }, {
+        _id: "22",
+        name: "Sneakers",
+        image: shoe_2,
+        price: 2200,
+        description: "Versatile sneakers for modern style and all-day comfort",
+        category: "Shoe"
+    }, {
+        _id: "23",
+        name: "Loafers",
+        image: shoe_3,
+        price: 1000,
+        description: "Classic loafers for effortless elegance and comfort",
+        category: "Shoe"
+    }, {
+        _id: "24",
+        name: "Canvas",
+        image: shoe_4,
+        price: 1200,
+        description: "Lightweight canvas shoes for stylish, everyday wear",
+        category: "Shoe"
+    },
+    {
+        _id: "25",
+        name: "Toned Shorts",
+        image: shorts_1,
+        price: 220,
+        description: "Comfortable toned shorts for active lifestyles",
+        category: "Shorts"
+    },
+    {
+        _id: "26",
+        name: "Jean Shorts",
+        image: shorts_2,
+        price: 180,
+        description: "Classic jean shorts for casual comfort and durability",
+        category: "Shorts"
+    }, {
+        _id: "27",
+        name: "Jean Shorts",
+        image: shorts_3,
+        price: 160,
+        description: "Timeless jean shorts for casual comfort and enduring style",
+        category: "Shorts"
+    }, {
+        _id: "28",
+        name: "Casual Shorts",
+        image: shorts_4,
+        price: 240,
+        description: "Comfortable casual shorts for warm-weather adventures",
+        category: "Shorts"
+    }, {
+        _id: "29",
+        name: "Basic T-Shirt",
+        image: t_shirt_1,
+        price: 100,
+        description: "Essential comfort, versatile and stylish",
+        category: "T-shirt"
+    }, {
+        _id: "30",
+        name: "Round Neck T-Shirt",
+        image: t_shirt_2,
+        price: 150,
+        description: "Classic comfort for everyday style",
+        category: "T-shirt"
+    }, {
+        _id: "31",
+        name: "Designer T-Shirt",
+        image: t_shirt_3,
+        price: 400,
+        description: "Unique patterns and premium materials define our designer tees",
+        category: "T-shirt"
+    }, {
+        _id: "32",
+        name: "Printed T-Shirt",
+        image: t_shirt_4,
+        price: 500,
+        description: "Vibrant designs with high-quality printing",
+        category: "T-shirt"
+    }
+]
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Bringing your anime dreams to your doorstep</h2>
+            <p>Dive into the realm of anime with each order made on our platform, where fantasies are wrapped and shipped straight to your door.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
